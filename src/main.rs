4fc538115b0d15015c2c319650f7bfde81210022
genga6 <gengaru617science@gmail.com>
2026-01-28 08:49:36 +0000

    Add make_move func_

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,29 @@ use minishogi_rs::{board, rules, ui};
 
 fn main() {
     println!("=== 5×5 Shogi Start ===");
-    let b = board::init();
+
+    let mut b = board::init();
     ui::print_board(&b);
 
-    println!("--- Sente's Legal Moves ---");
+    println!("\n--- Sente's Legal Moves ---");
     let moves = rules::generate_legal_moves(&b, board::Player::Sente);
+    println!("Total: {} moves", moves.len());
 
-    for mv in moves {
-        match mv {
+    if let Some(first_move) = moves.first() {
+        println!("\n--- Making first move ---");
+
+        match first_move {
             rules::Move::To(from, to) => {
                 println!("Move: ({}, {}) -> ({}, {})", from.x, from.y, to.x, to.y);
             }
         }
+
+        b = rules::make_move(&b, *first_move);
+        println!("\n--- After move ---");
+        ui::print_board(&b);
+
+        println!("\n--- Gote's Legal Moves ---");
+        let gote_moves = rules::generate_legal_moves(&b, board::Player::Gote);
+        println!("Total: {} moves", gote_moves.len());
     }
 }
